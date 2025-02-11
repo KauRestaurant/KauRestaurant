63271e711f222eac6c90e98f110237eb2a68443d
Abdulaziz Almalki <abdulazizalmalki54@gmail.com>
2025-02-11 21:23:11 +0300

    Fixed routing buttons on index page and added login and register pages accessible from the header

---
+++ Areas/Identity/Data/KauRestaurantUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace KauRestaurant.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the KauRestaurantUser class
+public class KauRestaurantUser : IdentityUser
+{
+}
+
