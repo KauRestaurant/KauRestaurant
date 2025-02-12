3f0c202e8988a212df6ef9b3156a78393f77a3b0
Ayman Aziz <aaziz0010@stu.kau.edu.sa>
2025-02-12 16:48:51 +0300

    Home changed to User

--- Program.cs
+++ Program.cs
@@ -34,7 +34,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/User/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -48,7 +48,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=User}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
